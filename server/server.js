5ecd73204bf0a95a7368d4352b8caa84c7e5e14a
Hugo-huy2004 <huylggcs230377@fpt.edu.vn>
2026-05-19 20:18:51 +0700

    fix: server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 8081;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hugo_wishpax';
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }
+));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
