5fa184428cbe561d4cc5cebfafd2df819e42cfff
Hugo-huy2004 <huylggcs230377@fpt.edu.vn>
2026-05-19 20:22:55 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -14,12 +14,17 @@ const PORT = process.env.PORT || 8081;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/hugo_wishpax';
 
 // Middleware
-app.use(cors(
-  {
-    origin: process.env.CLIENT_URL,
-    credentials: true,
+const allowedOrigins = process.env.CLIENT_URLS.split(",");
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Blocked by CORS"));
+    }
   }
-));
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
